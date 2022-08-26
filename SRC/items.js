c0e3b9da8ea1b68883679e92c660996958f2a196
Richmond-Andoh <richmondanoh00@gmail.com>
2022-08-26 16:20:16 +0000

    restructuring items.js

--- SRC/items.js
+++ SRC/items.js
@@ -1,6 +1,5 @@
 "use strict"
-let itemStore = [
-    {
+let itemStore = [{
         id: "first",
         title: "Casual Shirt",
         price: 40,
@@ -19,87 +18,87 @@ let itemStore = [
         title: "Mens Suit",
         price: 50,
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
-        img: "images/suit.jpg"
+        img: "images/Suit.jpg"
     },
 
     {
-    id: "fourth",
-    title: "Air",
-    price: 40,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
-    img: "images/sneaker-1.jpg"
+        id: "fourth",
+        title: "Air",
+        price: 40,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
+        img: "images/Sneaker-1.jpg"
     },
     {
-    id: "fifth",
-    title: "Air",
-    price: 60,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
-    img: "images/sneaker-2.jpg"
+        id: "fifth",
+        title: "Air",
+        price: 60,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
+        img: "images/Sneaker-2.jpg"
     },
     {
-    id: "sixth",
-    title: "Air Max",
-    price: 50,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
-    img: "images/sneaker-3.jpg"
-    },
-
-    {
-    id: "seventh",
-    title: "Sneaker-7",
-    price: 40,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
-    img: "images/Sneaker-7.jpg"
-    },
-    {
-    id: "eighth",
-    title: "Sneaker-8",
-    price: 60,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
-    img: "images/Sneaker-8.jpg"
-    },
-    {
-    id: "nineth",
-    title: "Sneaker-9",
-    price: 50,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
-    img: "images/Sneaker-9.jpg"
+        id: "sixth",
+        title: "Air Max",
+        price: 50,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
+        img: "images/sneaker-3.jpg"
     },
 
     {
-    id: "tenth",
-    title: "Sneaker-10",
-    price: 40,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
-    img: "images/Sneaker-10.jpg"
+        id: "seventh",
+        title: "Sneaker-7",
+        price: 40,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
+        img: "images/Sneaker-7.jpg"
     },
     {
-    id: "eleventh",
-    title: "PGM",
-    price: 60,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
-    img: "images/PGM.jpg"
+        id: "eighth",
+        title: "Sneaker-8",
+        price: 60,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
+        img: "images/Sneaker-8.jpg"
     },
     {
-    id: "twelve",
-    title: "PGM-1",
-    price: 50,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
-    img: "images/PGM-1.jpg"
+        id: "nineth",
+        title: "Sneaker-9",
+        price: 50,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
+        img: "images/Sneaker-9.jpg"
+    },
+
+    {
+        id: "tenth",
+        title: "Sneaker-10",
+        price: 40,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
+        img: "images/Sneaker-10.jpg"
     },
     {
-    id: "thirteen",
-    title: "Pink-Heel",
-    price: 60,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
-    img: "images/Pink-heel.jpg"
+        id: "eleventh",
+        title: "PGM",
+        price: 60,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
+        img: "images/PGM.jpg"
     },
     {
-    id: "fourteen",
-    title: "Black-Heel",
-    price: 50,
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
-    img: "images/Black-heel.jpg"
+        id: "twelve",
+        title: "PGM-1",
+        price: 50,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
+        img: "images/PGM-1.jpg"
+    },
+    {
+        id: "thirteen",
+        title: "Pink-Heel",
+        price: 60,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
+        img: "images/Pink-heel.jpg"
+    },
+    {
+        id: "fourteen",
+        title: "Black-Heel",
+        price: 50,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, corporis!",
+        img: "images/Black-heel.jpg"
     },
     {
         id: "fifteen",
